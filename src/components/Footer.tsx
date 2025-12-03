b797bacd5590a5f3f88f489037b4ed26c98c8773
o66ns <tttrotzdem@gmail.com>
2025-12-03 17:04:44 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,8 @@
 import { FC } from "react";
-import { Link } from "react-router-dom";
 
 const Footer: FC = () => (
   <div className="bg-black text-white p-4 text-center mt-auto">
-    <div>&copy; Copyright | All Rights Reserved</div>
-    <div>
-      <Link
-        to="https://alim1496.github.io/"
-        className="hover:underline hover:font-bold opacity-85 hover:opacity-100"
-      >
-        M A Alim
-      </Link>
-    </div>
+    <div>&copy; Copyright 2025 | All Rights Reserved</div>
   </div>
 );
 
